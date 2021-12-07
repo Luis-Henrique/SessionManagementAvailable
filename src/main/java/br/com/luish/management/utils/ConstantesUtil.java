cac2c9c17c258a9b56a7f29a1298b174ccaa4169
Luis Henrique <luish.silveira@hotmail.com>
2021-12-07 19:02:22 -0300

    Added configurations to profiles

--- src/main/java/br/com/luish/management/utils/ConstantesUtil.java
+++ src/main/java/br/com/luish/management/utils/ConstantesUtil.java
@@ -6,8 +6,4 @@ public class ConstantesUtil {
 	public static final String MSG_CPF_VALID 		= "ABLE_TO_VOTE";
 	public static final int ONE_MINUTE_IN_MILLIS  	= 60000;
 	
-	public static final String URL_MYSQL			= "jdbc:mysql://us-cdbr-east-04.cleardb.com:3306/heroku_50ecd93e36f8fd2";
-	public static final String USER					= "b44694ad1e080b";
-	public static final String PASSWORD				= "53dff8da";
-	
 }
